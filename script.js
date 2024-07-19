ce5379785c8bbe38cf434d2ad3c9cd51d59b6d4b
Anna Tasheva <anna.tasheva90@gmail.com>
2024-07-19 14:56:44 +0100

    Created a Project Section

--- script.js
+++ script.js
@@ -1,2 +1,58 @@
+//PORTFOLIO SECTION
+
+const projectsData = [
+  {
+    title: "Project 1",
+    image: "./images/project.png",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    title: "Project 2",
+    image: "./images/project.png",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    title: "Project 3",
+    image: "./images/project.png",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    title: "Project 4",
+    image: "./images/project.png",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    title: "Project 5",
+    image: "./images/project.png",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+];
+
+const projectContainer = document.getElementById("project-container");
+
+projectsData.forEach((project) => {
+  const projectCard = document.createElement("div");
+  projectCard.classList.add("project-card");
+
+  projectCard.innerHTML = `
+        <div class="project-image">
+            <img src="${project.image}" alt="Rocket Image">
+        </div>
+        <div class="project-details">
+            <p class="project-title">${project.title}</p>
+            <p class="project-description">${project.description}</p>
+        </div>
+    `;
+
+  projectContainer.appendChild(projectCard);
+});
+
+//FOOTER with YEAR
+
 const yearElement = document.querySelector("#year");
 yearElement.textContent = new Date().getFullYear();
